7e0182d79ed4b379c07e951b3620b7065ee8077b
ankitvashisht12 <ankitvashisht12@gmail.com>
2019-10-29 18:31:08 +0530

    added more image and test data

--- location_details/lib/models/location.dart
+++ location_details/lib/models/location.dart
@@ -1,15 +1,28 @@
 import 'location_fact.dart';
 
 class Location{
+  final int id;
   final String name;
   final String imagePath;
   final List<LocationFact> facts;
 
-  Location(this.name, this.imagePath, this.facts);
+  Location(this.id, this.name, this.imagePath, this.facts);
 
   static List<Location> fetchAll() {
     return [
-      Location('Arashiyama Bamboo Grove', 'assets/images/colorful.jpg', [
+      Location(1, 'Arashiyama Bamboo Grove', 'assets/images/colorful.jpg', [
+        LocationFact('Summary',
+            'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm.'),
+        LocationFact('How to Get There',
+            'Kyoto airport, with several terminals, is located 16 kilometres south of the city and is also known as Kyoto. Kyoto can also be reached by transport links from other regional airports.'),
+      ]),
+      Location(2, 'Arashiyama Bamboo Grove', 'assets/images/car.jpg', [
+        LocationFact('Summary',
+            'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm.'),
+        LocationFact('How to Get There',
+            'Kyoto airport, with several terminals, is located 16 kilometres south of the city and is also known as Kyoto. Kyoto can also be reached by transport links from other regional airports.'),
+      ]),
+      Location(3, 'Mount Fuji', 'assets/images/moon.jpg', [
         LocationFact('Summary',
             'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm.'),
         LocationFact('How to Get There',
